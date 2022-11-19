46adad93a5f84e3b6587da9514fb3a7eff842617
Rjunior18 <robert.gamez.quinonez@utelvt.edu.ec>
2022-11-19 17:43:52 -0500

    modificacion

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ document.getElementById("elnombre").innerHTML="Gamez Robert";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
- document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-kevin.php'>Rivera Fabricio</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-kevin.php'>Kevin Aguilar</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> ";
  </script>
  
 <?php
